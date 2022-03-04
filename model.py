505adabe7dfa8e17d635ee835a5e29f9d666f2a3
Lipsa-Tech <india.lipsa@gmail.com>
2022-03-04 07:01:19 +0530

    added new ml code

--- model.py
+++ model.py
@@ -1,1 +1,9 @@
 print("code for ml model")
+def model1():
+    print("adding code for model 1 algorithm")
+
+def model2():
+    print("adding code for model 2 algorithm")
+
+def model3():
+    print("adding code for model 3 algorithm")
